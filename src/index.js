0b00e96f76cd5ad270e14f15f86d255a12429dbd
Martin Fieber <info@martin-fieber.de>
2018-02-12 09:19:03 +0100

    refactor: make api compatible to v1
    This change introduces the murmurHash as an alias to murmurhash2_x86_32 to ensure compatibility to
    v1 of this library.

--- src/index.js
+++ src/index.js
@@ -8,3 +8,4 @@ const murmurhash2_x86_32 = require('./murmurhash2_x86_32');
 
 exports.aleaRNGFactory = aleaRNGFactory;
 exports.murmurhash2_x86_32 = murmurhash2_x86_32;
+exports.murmurHash = murmurhash2_x86_32; // Compatibility to v1
