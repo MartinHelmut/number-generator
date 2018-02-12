9fcc94fcb554c9e50951bc4db91a702feb65e481
Martin Fieber <martin.fieber@sinnerschrader.com>
2018-02-12 09:15:24 +0100

    feat: split methods to single functions
    Not only are the functions now splited into separate functions, the build process is aligned as
    well. The documentation got an overall fix, jsdoc compatibility is better and a lot of
    configurations are now included inside the package.json file.

---
+++ src/aleaRNGFactory.d.ts
@@ -0,0 +1,3 @@
+/*! The MIT License (MIT) - Copyright (c) 2016-2018 Martin Helmut Fieber */
+import { aleaRNGFactory } from './index';
+export default aleaRNGFactory;
---
+++ src/murmurhash2_x86_32.d.ts
@@ -0,0 +1,3 @@
+/*! The MIT License (MIT) - Copyright (c) 2016-2018 Martin Helmut Fieber */
+import { murmurhash2_x86_32 } from './index';
+export default murmurhash2_x86_32;
