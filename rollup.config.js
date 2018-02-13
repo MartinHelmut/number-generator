dc623f3eba880d1e3e31cf5caf45a7e640e0e05a
Martin Fieber <info@martin-fieber.de>
2018-02-13 12:22:48 +0100

    build: use rollup and closure to bundle single functions as well

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,60 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import closure from 'rollup-plugin-closure-compiler-js';
+import filesize from 'rollup-plugin-filesize';
 
-export default {
-    input: 'src/index.js',
-    output: {
-        file: 'lib/index.js',
+const babelOptions = {
+    babelrc: false,
+    exclude: 'node_modules/**',
+    presets: [
+        [
+            'env',
+            {
+                modules: false
+            }
+        ]
+    ],
+    plugins: ['external-helpers']
+};
+
+const closureOptions = {
+    compilationLevel: 'SIMPLE',
+    languageOut: 'ECMASCRIPT5_STRICT',
+    env: 'CUSTOM',
+    processCommonJsModules: false,
+    applyInputSourceMaps: true,
+    createSourceMap: true
+};
+
+function createBundleConfig(fileName, output = {}) {
+    return {
+        input: `src/${fileName}.js`,
+        output: Object.assign(
+            {
+                file: `lib/${fileName}.js`,
+                format: 'cjs',
+                sourcemap: true,
+                sourcemapFile: `lib/${fileName}.js.map`
+            },
+            output
+        ),
+        plugins: [
+            resolve(),
+            babel(babelOptions),
+            closure(closureOptions),
+            filesize({
+                render: (_, size, gzip) =>
+                    `Bundle size: ${size}, Gzipped size: ${gzip}`
+            })
+        ]
+    };
+}
+
+export default [
+    createBundleConfig('index', {
         format: 'umd',
         name: 'numberGenerator'
-    },
-    plugins: [
-        resolve(),
-        babel({
-            babelrc: false,
-            exclude: 'node_modules/**',
-            presets: [
-                [
-                    'env',
-                    {
-                        modules: false
-                    }
-                ]
-            ],
-            plugins: ['external-helpers']
-        })
-    ]
-};
+    }),
+    createBundleConfig('aleaRNGFactory'),
+    createBundleConfig('murmurhash2_x86_32')
+];
