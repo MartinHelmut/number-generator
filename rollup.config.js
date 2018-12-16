3aa0d6e0373d80123048d62086420cc060eb2259
Martin Fieber <info@martin-fieber.de>
2018-12-16 19:29:34 +0100

    build(rollup): use cpy plugin to copy d.ts files to lib

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,37 @@
 import babel from 'rollup-plugin-babel';
 import clear from 'rollup-plugin-clear';
+import copy from 'rollup-plugin-cpy';
 import filesize from 'rollup-plugin-filesize';
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 function createBundleConfig(input, output = {}) {
+    const targetDir = 'lib';
     const fileName = input.split('/').pop();
 
     return {
         input: `src/${input}.js`,
         output: Object.assign(
             {
-                file: `lib/${fileName}.js`,
+                file: `${targetDir}/${fileName}.js`,
                 format: 'cjs',
                 sourcemap: true,
-                sourcemapFile: `lib/${fileName}.js.map`
+                sourcemapFile: `${targetDir}/${fileName}.js.map`
             },
             output
         ),
         plugins: [
-            clear({ targets: ['lib'] }),
+            clear({ targets: [targetDir] }),
             resolve(),
             babel(),
             terser(),
             filesize({
                 render: (_, __, { bundleSize, gzipSize }) =>
                     `Bundle size: ${bundleSize}, Gzipped size: ${gzipSize}`
+            }),
+            copy({
+                files: ['src/**/*.d.ts'],
+                dest: targetDir
             })
         ]
     };
