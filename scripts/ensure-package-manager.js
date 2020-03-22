332ef598cdb844a5e487a797d668b553611282a0
Martin Helmut Fieber <info@martin-fieber.de>
2020-03-22 11:13:51 +0100

    build: add prepare script to ensure package manager
    This re-introduces the local check that pnpm is used for development. This was duo to a bug removed.
    
    fix #592

--- scripts/ensure-package-manager.js
+++ scripts/ensure-package-manager.js
@@ -1,6 +1,12 @@
 "use strict";
 
-if (!process.env.npm_config_user_agent.startsWith("pnpm/")) {
+const argv = JSON.parse(process.env.npm_config_argv) || { original: [] };
+const isPublishCommand = argv.original.includes("publish");
+
+if (
+  !isPublishCommand &&
+  !process.env.npm_config_user_agent.startsWith("pnpm/")
+) {
   console.log(
     "\x1b[31m%s\x1b[0m",
     "Error: Use `pnpm install` to install dependencies in this repository\n"
